91041a9580b537d0693dfad1f51d9b9d02ad76ae
David Chan <dchaan1@gmail.com>
2023-06-12 15:09:35 -0700

    change user to currentUser

--- frontend/pawzon.jsx
+++ frontend/pawzon.jsx
@@ -5,10 +5,19 @@ import Root from './components/root'
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
-  
+
   // testing
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+  }
 
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, root);
