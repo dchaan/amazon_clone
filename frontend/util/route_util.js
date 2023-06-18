10344fec27f1e761a910c26553783a6aecdebb38
David Chan <dchaan1@gmail.com>
2023-06-18 11:07:07 -0700

    Login component

--- frontend/util/route_util.js
+++ frontend/util/route_util.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Route, Redirect, withRouter } from "react-router";
+import { Route, Redirect } from "react-router";
+import { withRouter } from "react-router-dom"
 
 const Auth = ({ component: Component, path, loggedIn, exact }) => (
   <Route path={path} exact={exact} render={(props) =>
