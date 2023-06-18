b2a1503fe6711cccf4ddfc92fa98ae4e6995ea04
David Chan <dchaan1@gmail.com>
2023-06-18 11:18:13 -0700

    Update route util

--- frontend/util/route_util.js
+++ frontend/util/route_util.js
@@ -1,21 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { Route, Redirect } from "react-router";
-import { withRouter } from "react-router-dom"
+import { withRouter } from "react-router-dom";
+
+const mSTP = state => (
+  {loggedIn: Boolean(state.session.id)}
+);
 
 const Auth = ({ component: Component, path, loggedIn, exact }) => (
-  <Route path={path} exact={exact} render={(props) =>
-    !loggedIn ? <Component {... props} /> : <Redirect to='/' />
-  }
-  />
+  <Route path={path} exact={exact} render={(props) => !loggedIn ? <Component {... props} /> : <Redirect to='/' />}/>
 )
 
 const Protected = ({ component: Component, path, loggedIn, exact }) => (
-  <Route path={path} exact={exact} render={(props) =>
-    loggedIn ? <Component {...props} /> : <Redirect to="/login" />
-  } 
-  />
+  <Route path={path} exact={exact} render={(props) => loggedIn ? <Component {...props} /> : <Redirect to="/login" />} />
 )
 
-export const AuthRoute = withRouter(connect(mapStateToProps)(Auth))
-export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected))
+export const AuthRoute = withRouter(connect(mSTP, null)(Auth))
+export const ProtectedRoute = withRouter(connect(mSTP)(Protected))
